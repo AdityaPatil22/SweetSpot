4b4d94cf5a33a4c91cba1db74e70b59af947cd33
AdityaP7649 <adityapatil7649@gmail.com>
2024-11-01 13:21:36 +0530

    added frontend deployed path

--- Sweetspot-Backend/index.js
+++ Sweetspot-Backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173' || 'https://sweets-spot.netlify.app',
   credentials: true 
 }));
 
