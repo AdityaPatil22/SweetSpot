5e16c25741babd7edb4e264c5d99b02df1d040c3
AdityaP7649 <adityapatil7649@gmail.com>
2024-11-18 12:22:51 +0530

    Added new footer

--- Sweetspot-Frontend/src/components/Footer/Footer.jsx
+++ Sweetspot-Frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,38 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="container-fluid bg-dark text-white-50 footer-container">
-      
-    </div>
+    <footer
+      className="text-center text-lg-start"
+      style={{ backgroundColor: "#284b63" }}
+    >
+      <div className="container d-flex justify-content-center py-5">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+        >
+          <i className="fab fa-facebook-f"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+        >
+          <i className="fab fa-youtube"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+        >
+          <i className="fab fa-instagram"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+        >
+          <i className="fab fa-twitter"></i>
+        </button>
+      </div>
+    </footer>
   );
 }
 
