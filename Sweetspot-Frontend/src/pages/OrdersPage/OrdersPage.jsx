9137751064c6915397eb908ed58361b3ec0e4909
AdityaP7649 <adityapatil7649@gmail.com>
2024-11-18 12:22:51 +0530

    Added new footer

--- Sweetspot-Frontend/src/pages/OrdersPage/OrdersPage.jsx
+++ Sweetspot-Frontend/src/pages/OrdersPage/OrdersPage.jsx
@@ -22,35 +22,41 @@ function OrdersPage() {
       <Header />
       <PageTitle title="Orders" />
       <div className="orders-main-container container-fluid col-lg-9">
-        <table className="table table-styled">
-          <thead>
-            <tr>
-              <th scope="col">Sr No.</th>
-              <th scope="col">Order ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Mobile</th>
-              <th scope="col">Order Total</th>
-              <th scope="col">Order Date</th>
-              <th scope="col">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orderDetails.map((order, index) => (
-              <tr key={order._id}>
-                <th scope="row">{index + 1}</th>
-                <td>{order._id}</td>
-                <td>
-                  {order.firstName} {order.lastName}
-                </td>
-                <td>{order.mobile}</td>
-                <td>₹ {order.cartTotal}</td>
-                <td>{new Date(order.orderDate).toLocaleDateString("en-CA")}</td>
-                <td className="status status-completed">{order.orderStatus}</td>
+        <div style={{ maxHeight: "calc(10 * 50px)", overflowY: "auto" }}>
+          <table className="table table-styled">
+            <thead>
+              <tr>
+                <th scope="col">Sr No.</th>
+                <th scope="col">Order ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Mobile</th>
+                <th scope="col">Order Total</th>
+                <th scope="col">Order Date</th>
+                <th scope="col">Status</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {orderDetails.slice(0, 10).map((order, index) => (
+                <tr key={order._id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{order._id}</td>
+                  <td>
+                    {order.firstName} {order.lastName}
+                  </td>
+                  <td>{order.mobile}</td>
+                  <td>₹ {order.cartTotal}</td>
+                  <td>
+                    {new Date(order.orderDate).toLocaleDateString("en-CA")}
+                  </td>
+                  <td className="status status-completed">
+                    {order.orderStatus}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>        
       <Footer />
     </>
   );
