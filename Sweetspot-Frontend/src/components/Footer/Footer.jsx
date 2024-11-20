20f6e5e876619eda0d45317f47a1b46a3aa55935
AdityaP7649 <adityapatil7649@gmail.com>
2024-11-20 17:11:56 +0530

    Made UI changes on footer

--- Sweetspot-Frontend/src/components/Footer/Footer.jsx
+++ Sweetspot-Frontend/src/components/Footer/Footer.jsx
@@ -2,33 +2,25 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer
-      className="text-center text-lg-start"
-    >
+    <footer className="text-center text-lg-start">
       <div className="container d-flex justify-content-center py-5">
         <button
           type="button"
-          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+          className="btn btn-primary btn-lg btn-floating mx-2 footer-btn"
         >
-          <i className="fab fa-facebook-f"></i>
+          <i className="fa-brands fa-square-facebook"></i>
         </button>
         <button
           type="button"
-          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+          className="btn btn-primary btn-lg btn-floating mx-2 footer-btn"
         >
-          <i className="fab fa-youtube"></i>
+          <i className="fa-brands fa-square-instagram"></i>
         </button>
         <button
           type="button"
-          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
+          className="btn btn-primary btn-lg btn-floating mx-2 footer-btn"
         >
-          <i className="fab fa-instagram"></i>
-        </button>
-        <button
-          type="button"
-          className="btn btn-primary btn-lg btn-floating mx-2 rounded-circle"
-        >
-          <i className="fab fa-twitter"></i>
+          <i className="fa-brands fa-x-twitter"></i>
         </button>
       </div>
     </footer>
